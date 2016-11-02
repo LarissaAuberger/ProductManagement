ba20effd12dd36ecd34edc4275f142cf9663d3e0
Larissa Auberger <larissa.auberger@de.ibm.com>
2016-11-02 17:15:22 +0100

    changed record_scrap

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -41,7 +41,7 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
 
-	err := stub.PutState("hello_world", []byte(args[0]))
+	err := stub.PutState("document_scrap", []byte(args[0]))
 	if err != nil {
 		return nil, err
 	}
@@ -72,6 +72,11 @@ func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args 
 	if function == "read" { //read a variable
 		return t.read(stub, args)
 	}
+	
+	if function == "read_number" { //read a variable
+		return t.read_number(stub, args)
+	}
+	
 	fmt.Println("query did not find func: " + function)
 
 	return nil, errors.New("Received unknown function query: " + function)
@@ -98,6 +103,25 @@ func (t *SimpleChaincode) write(stub *shim.ChaincodeStub, args []string) ([]byte
 
 // read - query function to read key/value pair
 func (t *SimpleChaincode) read(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+	var key, jsonResp string
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+	}
+
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	return valAsbytes, nil
+}
+
+// read - query function to read key/value pair
+func (t *SimpleChaincode) read_number(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
 	var key, jsonResp string
 	var err error
 
