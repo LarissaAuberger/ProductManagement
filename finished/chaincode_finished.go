ca7467aef3a5f07ad43a1c71365fb5d980baecf4
Larissa Auberger <larissa.auberger@de.ibm.com>
2016-11-08 00:28:04 +0100

    first commit

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -208,14 +208,15 @@ func (t *SimpleChaincode) read(stub *shim.ChaincodeStub, args []string) ([]byte,
 	}
 
 	key = args[0]
-	// var location = args [1]
+	var location = args [1]
 
 	valAsbytes, err := stub.GetState(key)
 	var product Product
 	json.Unmarshal([]byte(args[0]), &product)
 
-// if product.Shipments[0].Destination != location
-	if valAsbytes == nil || len(valAsbytes) == 0  {
+ if product.Shipments[0].Destination != location {
+// if details.Destination != location {
+//	if valAsbytes == nil || len(valAsbytes) == 0  {
 		// WIoTP REST API --> event für Device "BCFakeDetector" eventtype "fake-alert" JSON {"PID":"<replace-me>","fake":"true"}
 		url := "http://20wql7.messaging.internetofthings.ibmcloud.com:1883/api/v0002/application/types/FakeDetector/devices/BCFakeDetector/events/fake-alert"
     //https://orgId.messaging.internetofthings.ibmcloud.com:8883/api/v0002/application/types/typeId/devices/deviceId/events/eventId
